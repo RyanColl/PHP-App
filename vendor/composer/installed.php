6b3be8fca99750fb1f981d28f91c4b639feffdef
Ryan <ryancollicutt22@gmail.com>
2021-04-05 04:17:56 -0700

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd9bb22f718475cedfc0bf97f831bb1e35eabfb55',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd9bb22f718475cedfc0bf97f831bb1e35eabfb55',
+    ),
+  ),
+);

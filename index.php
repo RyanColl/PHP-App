6a89430539b10c8d4719479b16504ff778962220
Ryan <ryancollicutt22@gmail.com>
2021-04-05 03:35:30 -0700

    rest

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
-<?php
- echo "Welcome!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Assignment 8</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" media="screen and (max-device-width: 750px)" href="media.css">
+    <script src="./jQuery2.js"></script>
+    <script src="./classes-jq.js"></script>
+  </head>
+<body>
+    <div class="header-wrap">
+        <header>
+            <div class="image">
+                <a href="#" class="img">
+                    <img src="./assets/comp3.gif">
+                </a>
+            </div>
+        </header>
+        <nav class="navbar">
+            <a href="#" class="about"><p>ABOUT</p></a>
+            <a href="#" class="projects"><p class="navpa">
+                PROJECTS
+                <div class="insert" id="p">
+                    <div class="abouthover">
+                        <ul>
+                            <li>
+                                one
+                            </li>
+                            <li>
+                                two
+                            </li>
+                            <li>
+                                three
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </p></a>
+            
+            <a href="#" class="goals"><p>GOALS</p></a>
+            <a href="#" class="interests"><p class="navpi">
+                INTERESTS
+                <div class="insert" id="i">
+                    <div class="interestshover">
+                        <ul>
+                            <li>
+                                one
+                            </li>
+                            <li>
+                                two
+                            </li>
+                            <li>
+                                three
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </p></a>
+            <a href="#" class="contact"><p>CONTACT</p></a>
+        </nav>
+        <h3>Hi, I’m Ryan!</h3>
+        <h3>I like making apps and meeting new people.</h3>
+    </div>
+    <div class="main-wrap">
+        <main>
+
+        </main>
+    </div>
+</body>
+</html>
---
+++ jQuery2.js
@@ -0,0 +1,39 @@
+$($(document).ready(() => {
+    $(".insert").fadeOut(1)
+    $(".projects").hover(handlerInAbout, handlerOutAbout)
+    $(".interests").hover(handlerInInterests, handlerOutInterests)
+    $(".about").on("click", () => {
+
+    })
+}))
+
+const handlerInAbout = () => {
+    let p = $(".navpa")
+    setCSS(p)
+    $("#p").fadeIn(1)
+    // $("#p").load("./stuff/abouthover.html")
+}
+const handlerOutAbout = () => {
+    let p = $(".navpa")
+    returnCSS(p)
+    $("#p").fadeOut(150)
+    // $(".abouthover").remove()
+}
+const handlerInInterests = () => {
+    let p = $(".navpi")
+    setCSS(p)
+    $("#i").fadeIn(1)
+    // $("#i").load("./stuff/interestshover.html")
+}
+const handlerOutInterests = () => {
+    let p = $(".navpi")
+    returnCSS(p)
+    $("#i").fadeOut(150)
+    // $(".interestshover").remove()
+}
+const setCSS = (p) => {
+    p.css("color", "rgb(153, 153, 153)")
+}
+const returnCSS = (p) => {
+    p.css("color", "#666")
+}

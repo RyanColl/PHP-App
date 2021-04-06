e47eff73a33859097b79c0902ae29aeca07603cc
Ryan <ryancollicutt22@gmail.com>
2021-04-05 18:09:58 -0700

    test-removing-media

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" media="screen and (max-device-width: 750px)" href="media.css">
+    <!-- <link rel="stylesheet" media="screen and (max-device-width: 750px)" href="media.css"> -->
     <script src="./jQuery.js"></script>
   </head>
 <body>
